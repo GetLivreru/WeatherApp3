c369bd36de13e1665b7c6d4a82e1013758c3248a
theadilet <110306539+GetLivreru@users.noreply.github.com>
2024-02-09 23:06:08 +0600

    add new func

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes");
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use("/", routes);
+
+app.listen(PORT, function(){
+    console.log("Server running on port: " + PORT);
+});
--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const axios = require("axios");
-const path = require("path");
 
-const PORT = 3000;
 const API_KEY = "94b07001ec1f4be8df8aa962a94b7dad";
-const app = express(); 
+const router = express.Router();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.get("/ping", function(req, res){
+router.get("/ping", function(req, res){
     res.status(200); 
     res.send("pong"); 
 });
 
-app.get("/weather/:city", function(req, res){
+router.get("/weather/:city", function(req, res){
     const city = req.params.city;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric&local=ru`;
 
@@ -36,7 +32,7 @@ app.get("/weather/:city", function(req, res){
     });
 });
 
-app.get("/wallet/:address", function(req, res){
+router.get("/wallet/:address", function(req, res){
     const address = req.params.address;
     const url = `https://api.blockcypher.com/v1/btc/main/addrs/${address}/full?limit=5`;
 
@@ -55,7 +51,7 @@ app.get("/wallet/:address", function(req, res){
     });
 });
 
-app.get("/BTC/price", function(req, res){
+router.get("/BTC/price", function(req, res){
     const url = 'https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD';
 
     axios.get(url)
@@ -73,7 +69,4 @@ app.get("/BTC/price", function(req, res){
     });
 });
 
-
-app.listen(PORT, function(){
-    console.log("Server running on port: " + PORT);
-});
+module.exports = router;
